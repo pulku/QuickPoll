af5dfb33c36df8e13789c95b5fbfb2ef5230a61a
pulku <pinarul@hotmail.com>
2016-03-28 02:58:33 +0300

    Added findByPoll method to get all votes for a poll

--- src/main/java/com/pulku/repository/VoteRepository.java
+++ src/main/java/com/pulku/repository/VoteRepository.java
@@ -1,10 +1,14 @@
 package com.pulku.repository;
 
 import com.pulku.domain.Vote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by pınar on 26.03.2016.
  */
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+
+    @Query(value = "select v.* from Option o, Vote v where o.POLL_ID = ?1 and v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+    public Iterable<Vote> findByPoll(Long pollId);
 }
