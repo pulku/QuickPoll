3dff306bb12f73c5128834492b3686e60dcfd19e
pulku <pinarul@hotmail.com>
2016-03-28 02:53:52 +0300

    Created OptionCount and VoteResult data transfer object

--- src/main/java/com/pulku/dto/VoteResult.java
+++ src/main/java/com/pulku/dto/VoteResult.java
@@ -1,7 +1,28 @@
 package com.pulku.dto;
 
+import java.util.Collection;
+
 /**
  * Created by pınar on 28.03.2016.
  */
 public class VoteResult {
+
+    private int totalvotes;
+    private Collection<OptionCount> results;
+
+    public int getTotalvotes() {
+        return totalvotes;
+    }
+
+    public void setTotalvotes(int totalvotes) {
+        this.totalvotes = totalvotes;
+    }
+
+    public Collection<OptionCount> getResults() {
+        return results;
+    }
+
+    public void setResults(Collection<OptionCount> results) {
+        this.results = results;
+    }
 }
