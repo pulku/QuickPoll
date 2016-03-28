0c1aa6bcc249e935320798131477dcc0ca1b7572
pulku <pinarul@hotmail.com>
2016-03-29 00:41:04 +0300

    Created QuickPollMvcConfigAdapter to set paging size to 5

--- src/main/java/com/pulku/QuickPollMvcConfigAdapter.java
+++ src/main/java/com/pulku/QuickPollMvcConfigAdapter.java
@@ -1,7 +1,24 @@
 package com.pulku;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
 /**
  * Created by pınar on 29.03.2016.
  */
-public class QuickPollMvcConfigAdapter {
+@Configuration
+public class QuickPollMvcConfigAdapter extends WebMvcConfigurerAdapter {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
+        // Normally default size 20, set the default size to 5
+        phmar.setFallbackPageable(new PageRequest(0, 5));
+        argumentResolvers.add(phmar);
+        super.addArgumentResolvers(argumentResolvers);
+    }
 }
