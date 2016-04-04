8a4aafc31cbceb03b56473f36c7a84e7f43c85a1
pulku <pinarul@hotmail.com>
2016-04-04 22:43:06 +0300

    Added AuthenticationManager Bean to securityconfig

--- src/main/java/com/pulku/SecurityConfig.java
+++ src/main/java/com/pulku/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.pulku;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
