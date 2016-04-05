2d17a346316aa965af0d23f46f875c14e94f1234
pulku <pinarul@hotmail.com>
2016-04-05 17:22:47 +0300

    Created QuickPollClient and added getPollById method using resttemplate

--- src/main/java/com/pulku/client/QuickPollClient.java
+++ src/main/java/com/pulku/client/QuickPollClient.java
@@ -1,7 +1,23 @@
 package com.pulku.client;
 
+import com.pulku.domain.Poll;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by pınar on 5.04.2016.
  */
 public class QuickPollClient {
+
+    private static final String QUICK_POLL_URI_V1 = "http://localhost:8080/v1/polls";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public Poll getPollById(Long pollId) {
+        return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/" + pollId, Poll.class, pollId);
+    }
+
+    public static void main(String[] args) {
+        QuickPollClient client = new QuickPollClient();
+        Poll poll = client.getPollById(1L);
+        System.out.println(poll);
+    }
 }
