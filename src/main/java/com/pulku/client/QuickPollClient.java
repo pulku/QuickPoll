662f14d73d40b1b530cc355d2b55d42c545591cc
pulku <pinarul@hotmail.com>
2016-04-05 18:03:25 +0300

    Added getAllPolls method to QuickPollClient

--- src/main/java/com/pulku/client/QuickPollClient.java
+++ src/main/java/com/pulku/client/QuickPollClient.java
@@ -1,7 +1,12 @@
 package com.pulku.client;
 
 import com.pulku.domain.Poll;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by pınar on 5.04.2016.
@@ -15,9 +20,16 @@ public class QuickPollClient {
         return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/" + pollId, Poll.class, pollId);
     }
 
+    public List<Poll> getAllPolls() {
+        ParameterizedTypeReference<List<Poll>> responseType = new ParameterizedTypeReference<List<Poll>>() {};
+        ResponseEntity<List<Poll>> responseEntity = restTemplate.exchange(QUICK_POLL_URI_V1, HttpMethod.GET, null, responseType);
+        List<Poll> allPolls = responseEntity.getBody();
+        return allPolls;
+    }
+
     public static void main(String[] args) {
         QuickPollClient client = new QuickPollClient();
-        Poll poll = client.getPollById(1L);
-        System.out.println(poll);
+        List<Poll> polls = client.getAllPolls();
+        System.out.println(polls);
     }
 }
