9c125c5e2589e483293743223558fff3b06dae11
pulku <pinarul@hotmail.com>
2016-04-06 15:28:14 +0300

    Added update, delete and create poll methods to QuickPollClient

--- src/main/java/com/pulku/client/QuickPollClient.java
+++ src/main/java/com/pulku/client/QuickPollClient.java
@@ -1,12 +1,16 @@
 package com.pulku.client;
 
+import com.pulku.domain.Option;
 import com.pulku.domain.Poll;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by pınar on 5.04.2016.
@@ -17,7 +21,7 @@ public class QuickPollClient {
     private RestTemplate restTemplate = new RestTemplate();
 
     public Poll getPollById(Long pollId) {
-        return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/" + pollId, Poll.class, pollId);
+        return restTemplate.getForObject(QUICK_POLL_URI_V1 + "/{pollId}", Poll.class, pollId);
     }
 
     public List<Poll> getAllPolls() {
@@ -27,9 +31,44 @@ public class QuickPollClient {
         return allPolls;
     }
 
+    public URI createPoll(Poll poll) {
+        return restTemplate.postForLocation(QUICK_POLL_URI_V1, poll);
+    }
+
+    public void updatePoll(Poll poll) {
+        restTemplate.put(QUICK_POLL_URI_V1 + "/{pollId}", poll, poll.getId());
+    }
+
+    public void deletePoll(Long pollId) {
+        restTemplate.delete(QUICK_POLL_URI_V1 + "/{pollId}", pollId);
+    }
+
     public static void main(String[] args) {
         QuickPollClient client = new QuickPollClient();
+
+        Poll poll = client.getPollById(1L);
+        System.out.println(poll);
+
+
+
+        Poll newPoll = new Poll();
+        newPoll.setQuestion("What is your favorite color?");
+        Set<Option> options = new HashSet<>();
+        newPoll.setOptions(options);
+
+        Option option1 = new Option();
+        option1.setValue("Red");
+        options.add(option1);
+
+        Option option2 = new Option();
+        option2.setValue("Blue");
+        options.add(option2);
+
+        URI pollLocation = client.createPoll(newPoll);
+        System.out.println("Newly created poll location : " + pollLocation);
+
         List<Poll> polls = client.getAllPolls();
         System.out.println(polls);
+
     }
 }
--- src/main/java/com/pulku/domain/Option.java
+++ src/main/java/com/pulku/domain/Option.java
@@ -1,9 +1,6 @@
 package com.pulku.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Option {
